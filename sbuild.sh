49724f0e29fef55bb572e3c15bb8469c62e15806
Stratos Karafotis <stratosk@semaphore.gr>
2013-09-01 22:32:35 +0300

    1.5.8
    Signed-off-by: Stratos Karafotis <stratosk@semaphore.gr>

--- sbuild.sh
+++ sbuild.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BASE_SEMA_VER="Semaphore_N4_1.5.0"
+BASE_SEMA_VER="Semaphore_N4_1.5.8"
 VER=""
 SEMA_VER=$BASE_SEMA_VER$VER
 
@@ -29,6 +29,7 @@ MODULES_DIR=../mako_initramfs_4.3/lib/modules
 KERNEL_DIR=`pwd`
 OUTPUT_DIR=output/
 CWM_DIR=cwm/
+CWM_ANY_DIR=cwm_any/
 
 echo "LOCALVERSION="$LOCALVERSION
 echo "CROSS_COMPILE="$CROSS_COMPILE
@@ -38,12 +39,15 @@ echo "MODULES_DIR="$MODULES_DIR
 echo "KERNEL_DIR="$KERNEL_DIR
 echo "OUTPUT_DIR="$OUTPUT_DIR
 echo "CWM_DIR="$CWM_DIR
+echo "CWN_ANY_DIR="$CWM_ANY_DIR
 
 #make -j16 modules
 make -j16 > /dev/null
 
 rm `echo $MODULES_DIR"/*"`
+rm `echo ../$CWM_ANY_DIR"system/lib/modules/*"`
 find $KERNEL_DIR -name '*.ko' -exec cp -v {} $MODULES_DIR \;
+find $KERNEL_DIR -name '*.ko' -exec cp -v {} ../$CWM_ANY_DIR"system/lib/modules/" \;
 cd $INIT_DIR
 find . \( ! -regex '.*/\..*' \) | cpio -o -H newc -R root:root | gzip -9 > ../initrd.img
 cd  $KERNEL_DIR
@@ -57,6 +61,7 @@ cd  $KERNEL_DIR
 #cd ../../../
 
 cp arch/arm/boot/zImage ../boot.img
+cp arch/arm/boot/zImage ../$CWM_ANY_DIR/kernel/
 cd ../
 ./mkbootimg
 
@@ -64,6 +69,11 @@ cp boot.img $CWM_DIR
 cd $CWM_DIR
 zip -r `echo $SEMA_VER`.zip *
 mv  `echo $SEMA_VER`.zip ../$OUTPUT_DIR
+
+cd ../
+cd $CWM_ANY_DIR
+zip -r `echo $SEMA_VER`_any.zip *
+mv  `echo $SEMA_VER`_any.zip ../$OUTPUT_DIR
 
 DATE_END=$(date +"%s")
 echo
