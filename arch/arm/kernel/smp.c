359db039ad1b727b79b405bb3c599a50af776da3
hellsgod <hellsgod@gmx.ch>
2014-09-19 16:06:45 +0200

    Revert "ARM: 7011/1: Add ARM cpu topology definition"
    This reverts commit b1a476bd3c799914d36f6130a1827245798b4035.

--- arch/arm/kernel/smp.c
+++ arch/arm/kernel/smp.c
@@ -33,7 +33,6 @@
 #include <asm/cputype.h>
 #include <asm/exception.h>
 #include <asm/idmap.h>
-
 #include <asm/topology.h>
 #include <asm/mmu_context.h>
 #include <asm/pgtable.h>
@@ -305,10 +304,8 @@ static void smp_store_cpu_info(unsigned int cpuid)
 
 	cpu_info->loops_per_jiffy = loops_per_jiffy;
 
-
 	store_cpu_topology(cpuid);
 }
-
 
 /*
  * This is the secondary CPU boot entry.  We're using this CPUs
